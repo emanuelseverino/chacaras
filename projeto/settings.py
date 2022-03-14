332b414624e8d2c5637eee68fd3f25b2d03cb5c3
emanuelseverino <40120367+emanuelseverino@users.noreply.github.com>
2022-03-14 12:05:27 -0300

    Corrigiando

--- projeto/settings.py
+++ projeto/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'teste'
+    'teste',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
