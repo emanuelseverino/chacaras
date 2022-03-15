0c95e7ed3fc6593c70f5e87cbf568cfad87918eb
emanuelseverino <40120367+emanuelseverino@users.noreply.github.com>
2022-03-15 14:24:19 -0300

    Adicionando os Detalhes

--- chacara/urls.py
+++ chacara/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from chacara.views import ChacaraListView
+from chacara.views import ChacaraListView, ChacaraDetailView
 
 urlpatterns = [
     path('', ChacaraListView.as_view(), name='chacara-list'),
+    path('<int:pk>/', ChacaraDetailView.as_view(), name='chacara-detail'),
 ]
--- chacara/views.py
+++ chacara/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from chacara.models import Chacara
 
 
 class ChacaraListView(ListView):
+    model = Chacara
+
+
+class ChacaraDetailView(DetailView):
     model = Chacara
